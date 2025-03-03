5100435f89f44088f57b7197da2f6a5e9d8e5883
kateye1988 <kate.m.ye@icloud.com>
2025-03-03 06:45:06 -0800

    personalized mini prompt
    something fun
    
    Signed-off-by: kateye1988 <kate.m.ye@icloud.com>

---
+++ .github/workflows/hey smartypants.py
@@ -0,0 +1,8 @@
+print('Hey Smarty Pants!')
+person = input('this is a program to help you become a better english writer. are you ready?')
+print(' awesome! i am so glad you said' , person)
+print('tell me what your essay is about ')
+person1 = input(' ')
+print(' I love that topic')
+print(' that is a super intresting subject to write about') 
+ 
